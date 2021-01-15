bb7aad36ab7613caac3d9e90dd1c87b320c88e95
taeyoung24 <jtyoung24@naver.com>
2021-01-16 02:23:54 +0900

    bug fix and lots of func add

--- cafechatplus.js
+++ cafechatplus.js
@@ -1,7 +1,10 @@
 // chat info
-var role = {
+
+var memberList = [];
+
+const role = {
     manager: "마피",
-    subManager: "레이",
+    subManager: {id: "qmfkejtm2004", nick: "레이"},
     bot: "시에스"
 };
 
@@ -12,35 +15,64 @@ $("head")
 
 // var socket = new WebSocket("wss://talkwss.cafe.naver.com");
 
+function getMemeberInfo(userid) {
+    if (memberList.length > 0) {
+        memberList.forEach(v => {
+            if (v.memberId == userid) {
+                return v;
+            }
+        });
+        return null;
+    } else {
+        return null;
+    }
+}
 
 function getMemberList(callback) {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
-        try {
-            callback(JSON.parse(xhr.responseText.trim()));
-        } catch(err) {
-            callback([]);
-        }
+        memberList = JSON.parse(xhr.responseText.trim()).message.result.memberList;
     }
     xhr.open("get", "https://talk.cafe.naver.com/talkapi/v4/channels/661738695697/sync");
     xhr.send();
 }
 
+let lastChatSender = "";
+let lastChatEle = null;
+
 function hightlight_memberName() {
+    let delList = [];
     $("div.talk_info").each(function() {
         var tag_nick = $(this).children("strong");
-        var tag_bubble = $(this).children(".bubble")
+        var senderId = $(this).closest("li.log_friend").attr('targetid');
+        var tag_bubble = $(this).children("div.bubble")
         .css("font-size", "12px");
+
+        if ($(this).find("img").length == 0) {
+            if (senderId == lastChatSender) {
+                let msgHtml = tag_bubble.html();
+                lastChatEle.find("div.bubble").append(msgHtml);
+                delList.push($(this).closest("li"));
+                return true;
+            } else {
+                lastChatSender = senderId;
+                lastChatEle = $(this).closest("li");
+            }
+        }
+        
+
         var nick = tag_nick.text();
-        if (nick == role.subManager) {
-            tag_nick.css("color", "blue")
-            .text("[부매니저] " + nick);
+        if (senderId == role.subManager.id) {
+            tag_nick.css("color", "#fc0050").html(`<span>${role.subManager.nick}</span><span style="border:1px solid; border-radius: 4em; margin-left: 4px; font-size: 4px; padding: 1px 5px">부매니저</span>`);
         }else if (nick == role.bot) {
             tag_bubble
             .css("color", "#034036")
             .css("background-color", "#00deb9");
         }else {
-            tag_bubble.css("background-color", "#f2f2f2");
+            if ($(this).find("img").length == 0) {
+                tag_bubble.css("background-color", "#f2f2f2");
+            }
+            
         }
 
         tag_bubble.children("p.txt").each(function() {
@@ -59,13 +91,13 @@ function hightlight_memberName() {
                 } else return ref;
             };
             if (text.includes("<tag:\"")) {
-                
                 let a = replaceTagText(text);
                 $(this).html(a);
             }
         });
         
     });
+    delList.forEach(v => v.remove());
 }
 
 function config_chatbubble() {
@@ -90,7 +122,7 @@ var timer_j = setInterval(function() {
     hightlight_memberName();
     config_chatbubble();
     $("#thumb_text").appendTo("ul.list_talk");
-    $(".list_talk > li").css("padding", "2px 0");
+    $(".list_talk > li").css("padding", "4px 0");
 }, 10);
 
 var isWriting = false;
@@ -116,6 +148,25 @@ $("div.cont_chat").css("scroll-behavior", "smooth");
 $("a.btn_attach.btn_more").click(() => {$("span.link_form > label").click()});
 
 
+function convertTags(text) {
+    let sharps = text.match(/\#\S*/gi);
+    if (!sharps) sharps = [];
+    sharps.forEach(v => {
+        if (v.length > 1)
+            text = text.replace(v, `<tag:"${v.substr(1)}">`);
+    });
+
+
+    let mentions = text.match(/\@\S*/gi); 
+    if (!mentions) mentions = [];
+    mentions.forEach(v => {
+        if (v.length > 1)
+            text = text.replace(v, `<mention:"${v.substr(1)}">`);
+    });
+
+    return text;
+}
+
 function KeyBoardInputEvent(e) {
     // if (isWriting) {
     //     $("#text_inner").text(textbox.val());
@@ -133,16 +184,19 @@ function KeyBoardInputEvent(e) {
     //     }
         
     // }
-
-    if (e.keyCode == 49) {
-        // btn_sendmsg.get(0).click();
+    // console.log(e.key);
+    switch (e.keyCode) {
+        case 13:
+            textbox.val("aa");
+            break;
     }
-    if (e.key == "@" && e.shiftKey) {
-        getMemberList(data => {
-            console.log(data.message.result.memberList);
-        });
-    }
+    // btn_sendmsg.get(0).click();
+    textbox.val(convertTags(textbox.val()));
 }
 
-textbox.keydown(KeyBoardInputEvent);
-textbox.keyup(KeyBoardInputEvent);
+btn_sendmsg.click(function() {
+    // console.log("aa");
+    // textbox.val("aa");
+});
+
+textbox.keypress(KeyBoardInputEvent);
