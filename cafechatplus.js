19e4c4cd20460fa2d8ec3b80715856389cd8d3af
taeyoung24 <jtyoung24@naver.com>
2021-01-28 23:06:28 +0900

    bug fix

--- cafechatplus.js
+++ cafechatplus.js
@@ -1,10 +1,13 @@
 // chat info
+// 1분 새로고침
+// 맨션
+// 태그
 
 var memberList = [];
 
 const role = {
     manager: "마피",
-    subManager: {id: "qmfkejtm2004", nick: "레이"},
+    subManager: "레이",
     bot: "시에스"
 };
 
@@ -13,7 +16,15 @@ var $ = window.jQuery;
 $("head")
 .append('<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">');
 
-// var socket = new WebSocket("wss://talkwss.cafe.naver.com");
+var token = "";
+var channelNo = 661738695697;
+var userId = "jtyoung24";
+
+// var ws = new WebSocket(`wss://talkwss.cafe.naver.com/socket.io/?channelNo=${channelNo}&userId=${userId}&accessToken=${token}`);
+
+// ws.onopen = function(evt) {
+//     console.log(evt);
+// }
 
 function getMemeberInfo(userid) {
     if (memberList.length > 0) {
@@ -39,6 +50,7 @@ function getMemberList(callback) {
 
 let lastChatSender = "";
 let lastChatEle = null;
+let LastChatTime = 0;
 
 function convert_chats(eleList) {
     let delList = [];
@@ -46,22 +58,43 @@ function convert_chats(eleList) {
         var ele_chat = $(ele_v).find("div.talk_info");
         var ele_nick = $(ele_chat).children("strong"); 
         var senderId = $(ele_v).attr('targetid');
+        var nick = ele_nick.text();
         var ele_bubble = ele_chat.children("div.bubble")
-        .css("font-size", "12px")
-        .hover(() => {
-            $(ele_chat).children("div.bubble").animate({
-                boxShadow: "5px 5px 14px rgba(0, 0, 0, 0.16)"
+        let cht_time = Number($(ele_v).attr("timestamp"));
+        if (isNaN(cht_time)) cht_time = new Date().getTime();
+        
+        
+
+        // section of tag replace
+        ele_bubble.children("p.txt").each(function() {
+            var text = $(this).text();
+
+            // replace tag
+            let tag_color = "rgba(0, 144, 252, 0.5)"
+            if (nick == role.bot) tag_color = "rgba(255, 255, 255, 0.3)";
+            
+            let sharps = text.match(/\#\S*/gi); console.log(sharps);
+            if (!sharps) sharps = [];
+            sharps.forEach(v => {
+                console.log(v);
+                if (v.length > 1)
+                    var re = new RegExp(v, 'gi');
+                    text = text.replace(v, `<tag:"${v.substr(1)}">`);
             });
-            // $(this).children("div.bubble").css("box-shadow", "5px 5px 14px rgba(0, 0, 0, 0.16)");
-        }, () => {
-            $(ele_chat).children("div.bubble").css("box-shadow", "");
         });
 
+        // section of merging chat
         if ($(ele_chat).find("img").length == 0) {
             if (senderId == lastChatSender) {
-                let msgHtml = ele_bubble.html();
-                lastChatEle.find("div.bubble").append(msgHtml);
+                const msgHtml = ele_bubble.html();
+                if (LastChatTime <= cht_time) {
+                    lastChatEle.find("div.bubble").append(msgHtml);
+                    LastChatTime = cht_time;
+                }else {
+                    lastChatEle.find("div.bubble").prepend(msgHtml);
+                }
                 delList.push($(ele_v));
+                
                 return true;
             } else {
                 lastChatSender = senderId;
@@ -71,41 +104,38 @@ function convert_chats(eleList) {
             lastChatSender = "";
             lastChatEle = null;
         }
-        
 
-        var nick = ele_nick.text();
-        if (senderId == role.subManager.id) {
-            ele_nick.css("color", "#fc0050").html(`<span>${role.subManager.nick}</span><span style="border:1px solid; border-radius: 4em; margin-left: 4px; font-size: 4px; padding: 1px 5px">부매니저</span>`);
+
+        // section of highlight bubble
+        ele_bubble
+        .css("font-size", "12px")
+        .hover(() => {
+            // $(ele_chat).children("div.bubble").animate({
+            //     boxShadow: "5px 5px 14px rgba(0, 0, 0, 0.16)"
+            // });
+            $(ele_chat).children("div.bubble").css("box-shadow", "5px 5px 14px rgba(0, 0, 0, 0.16)");
+        }, () => {
+            $(ele_chat).children("div.bubble").css("box-shadow", "");
+        });
+
+
+        // section of  highlight account
+        
+        if (nick == role.manager) {
+            ele_nick.css("color", "#fc0050").html(`<span>${role.manager}</span><span style="border:1px solid; border-radius: 4em; margin-left: 4px; font-size: 4px; padding: 1px 5px">매니저</span>`);
+        }else if (nick == role.subManager) {
+            ele_nick.css("color", "#fc0050").html(`<span>${role.subManager}</span><span style="border:1px solid; border-radius: 4em; margin-left: 4px; font-size: 4px; padding: 1px 5px">부매니저</span>`);
         }else if (nick == role.bot) {
             ele_bubble
             .css("color", "#034036")
             .css("background-color", "#00deb9");
+
+            ele_nick.css("color", "#00deb9").html(`<span>[CS] ${role.bot}</span><span style="border:1px solid; border-radius: 4em; margin-left: 4px; font-size: 4px; padding: 1px 5px">봇</span>`);
         }else {
             if ($(ele_chat).find("img").length == 0) {
                 ele_bubble.css("background-color", "#f2f2f2");
             }
         }
-
-        ele_bubble.children("p.txt").each(function() {
-            var text = $(this).text();
-
-            // replace tag
-            let tag_color = "rgba(0, 144, 252, 0.5)"
-            if (nick == role.bot) tag_color = "rgba(255, 255, 255, 0.3)";
-            var replaceTagText = function(ref) {
-                if (ref.includes("<tag:\"")) {
-                    var targ = ref.split("<tag:\"")[1].split("\">")[0];
-                    var tagName = `<tag:\"${targ}\">`;
-                    targ = `<span style="background-color: ${tag_color}; border-radius: 4em; margin-left: 4px; font-size: 0.9em; padding: 3px 5px"><b>#${targ}</b></span>`;
-                    var replaced = ref.replace(tagName, targ);
-                    return replaceTagText(replaced);
-                } else return ref;
-            };
-            if (text.includes("<tag:\"")) {
-                let a = replaceTagText(text);
-                $(this).html(a);
-            }
-        });
         
         $(ele_chat).attr("isconverted", "true");
     });
@@ -114,19 +144,16 @@ function convert_chats(eleList) {
 
 
 function newChat(callback) {
-    var chatLen = 0;
     var timer_main = setInterval(function() {
         var newChatLen = $("div.talk_info").length;
-        if (chatLen != newChatLen) {
-            chatLen = newChatLen;
-            let retl = [];
-            $("div.talk_info").each((i, v) => {
-                if (!$(v).is("isconverted")) {
-                    retl.push($(v).closest("li"));
-                }
-            });
-            if (retl.length > 0) callback(retl);
-        }
+        let retl = [];
+        $("ul.list_talk > li").each((i, v) => {
+            var chat = $(v).find("div.talk_info");
+            if (!chat.is("[isconverted]")) {
+                retl.push(v);
+            }
+        });
+        if (retl.length > 0) callback(retl);
     }, 10);
 }
 
@@ -156,7 +183,7 @@ $("ul.list_talk").append(`<li id="thumb_text" class="log_my">
             <a href="#" class="link">공지로 등록</a></div></div></div></div></div>
             </li>`);
 $("#thumb_text").hide();
-$("div.cont_chat").css("scroll-behavior", "smooth");
+// $("div.cont_chat").css("scroll-behavior", "smooth");
 $("a.btn_attach.btn_more").click(() => {$("span.link_form > label").click()});
 
 
@@ -203,7 +230,7 @@ function KeyBoardInputEvent(e) {
             break;
     }
     // btn_sendmsg.get(0).click();
-    textbox.val(convertTags(textbox.val()));
+    // textbox.val(convertTags(textbox.val()));
 }
 
 btn_sendmsg.click(function() {
